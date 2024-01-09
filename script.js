a03919f6843674b6cd7d4d63a9f0a6859ba67062
Sigrídur Birna Matthíasdóttir <sigridurbirnamatthiasdottir@Sigridurs-MacBook-Pro.local>
2024-01-09 10:07:02 +0000

    feat : made 16 divs

---
+++ script.js
@@ -0,0 +1,9 @@
+const container = document.querySelector('#container');
+
+
+for (i = 0; i <= 16; i++) {
+    const content = document.createElement('div');
+    content.classList.add('content');
+    content.textContent = "hey";
+    container.append(content);
+}
